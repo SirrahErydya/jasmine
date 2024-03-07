69ab4adae93eacb575d6fa7a4e12d501019b6074
SirrahErydya <bo231@stud.uni-heidelberg.de>
2024-03-07 10:21:41 +0100

    Revert "Create Database models"
    This reverts commit dff08384d4e34d1e6486374cb7748300542e56c3.

--- jasmineapp/hips/models.py
+++ jasmineapp/hips/models.py
@@ -1,29 +1,3 @@
 from django.db import models
-import os
 
-
-# Create your models here
-def survey_path(instance, filename):
-    return os.path.join('/static/surveys/', instance.name, filename)
-
-
-def survey_cutout_path(instance, filename):
-    return os.path.join('/static/surveys/', instance.name, "cutouts", filename)
-
-
-def survey_model_path(instance, filename):
-    return os.path.join('/static/surveys/', instance.name, "models", filename)
-
-
-class Survey(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.CharField(max_length=128)
-    catalog = models.FileField(upload_to=survey_path)
-
-
-class DataPoint(models.Model):
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
-    identifier = models.CharField(max_length=128)
-    cutout = models.ImageField(upload_to=survey_cutout_path)
-    model_3d = models.FileField(upload_to=survey_model_path)
-    healpix_idx = models.IntegerField()
+# Create your models here.
